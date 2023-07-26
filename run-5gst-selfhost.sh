53a02a9ddebf5e544ee5dfd1680463b243133903
Kazakov Mihail <advancerman@gmail.com>
2023-07-26 18:41:44 +0300

    Add more obvious warning for non-tls selfhost

--- run-5gst-selfhost.sh
+++ run-5gst-selfhost.sh
@@ -17,7 +17,8 @@ function loadSecretFromFile {
     exit 1
   fi
 
-  if [[ ! -f "$1" ]]; then
+  if [[ ! -f "$1" ]]
+  then
     echo "## Generating secret key in file $1"
     openssl rand -hex 32 | cat > "$1"
   else
@@ -36,8 +37,16 @@ export BALANCER_SECRET_KEY="$retval"
 loadSecretFromFile .service.key
 export SERVICE_SECRET_KEY="$retval"
 
-echo "## WARNING: Starting server without TLS"
-export SERVICE_URL_SCHEME=http
+if [ -z "$SERVICE_URL_SCHEME" ]
+then
+  echo "##"
+  echo "## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "## WARNING: Starting server without TLS."
+  echo "##          Please, specify \"SERVICE_URL_SCHEME=https\" in environment variables to pass this warning"
+  echo "## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "##"
+  export SERVICE_URL_SCHEME=http
+fi
 
 echo "## Starting Docker Compose"
 docker compose up -d
