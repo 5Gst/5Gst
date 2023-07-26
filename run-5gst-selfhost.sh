30e283d84a22f69d49de5d0559d21e2fc922768c
Kazakov Mihail <advancerman@gmail.com>
2023-07-26 18:30:10 +0300

    Add scripts for selfhost deployment

--- run-5gst-selfhost.sh
+++ run-5gst-selfhost.sh
@@ -8,13 +8,36 @@ then
 fi
 
 export SERVER_HOST="$1"
+echo "## Using server host: '$SERVER_HOST'"
 
-echo "## Load Docker images from *.tar"
-docker load -i service.tar.gz
-docker load -i balancer.tar.gz
-docker load -i postgres.tar.gz
+function loadSecretFromFile {
+  if [ -z "$1" ]
+  then
+    echo "Expected file name as a first loadSecretFromFile function argument"
+    exit 1
+  fi
 
-echo "## Server host: " SERVER_HOST
+  if [[ ! -f "$1" ]]; then
+    echo "## Generating secret key in file $1"
+    openssl rand -hex 32 | cat > "$1"
+  else
+    echo "## Using already generated key from file $1"
+  fi
 
-echo "## Start Docker Compose"
+  retval="$(cat $1)"
+}
+
+loadSecretFromFile .db.pass
+export DB_PASS="$retval"
+
+loadSecretFromFile .balancer.key
+export BALANCER_SECRET_KEY="$retval"
+
+loadSecretFromFile .service.key
+export SERVICE_SECRET_KEY="$retval"
+
+echo "## WARNING: Starting server without TLS"
+export SERVICE_URL_SCHEME=http
+
+echo "## Starting Docker Compose"
 docker compose up -d
