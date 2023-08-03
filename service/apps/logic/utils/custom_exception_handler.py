183815f180b9e6869713f4d649aefe71eb145dc9
IceMeladrom <robertkryt@gmail.com>
2023-08-03 12:14:51 +0300

    #8. Changed exc handler

--- service/apps/logic/utils/custom_exception_handler.py
+++ service/apps/logic/utils/custom_exception_handler.py
@@ -7,22 +7,12 @@ logger = logging.getLogger(__name__)
 
 
 def custom_exception_handler(exc, context):
-    handlers = {
-        'BadRequest': _handler_badrequest_error,
-    }
-
     response = exception_handler(exc, context)
-
-    exception_class = exc.__class__.__name__
-
-    if exception_class in handlers:
-        logger.exception(traceback.format_exc())
-        return handlers[exception_class](exc, context, response)
-    return response
-
-
-def _handler_badrequest_error(exc, context, response):
     response.data = {
         'status_code': response.status_code,
         'status_text': response.status_text,
+        'default_detail': exc.default_detail,
     }
+
+    logger.exception(traceback.format_exc())
+    return response
