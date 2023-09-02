3d7b2355c35850193bdd0d60d8ba6490273bbd6b
Kazakov Mihail <advancerman@gmail.com>
2023-09-02 10:38:57 +0300

    Fix swagger schema

--- balancer/services/logic/iperf_measurement_history.py
+++ balancer/services/logic/iperf_measurement_history.py
@@ -38,7 +38,6 @@ class IperfMeasurementHistoryAPI:
     read_swagger_auto_schema = swagger_auto_schema(
         operation_description='Return measurement results',
         operation_id='read',
-        query_serializer=IperfMeasurementResultSerializer,
         manual_parameters=[
             openapi.Parameter(
                 'measurement_id',
